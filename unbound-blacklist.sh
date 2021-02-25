b75fe0a4e0e52e5b4029116e31c4a6213ba4179c
elasmo <elasmo@github>
2021-02-25 12:48:27 +0100

    Adding custom domains

--- unbound-blacklist.sh
+++ unbound-blacklist.sh
@@ -20,22 +20,20 @@ PATTERN="^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]
 BLACKLIST_CONF="$UNBOUND_CHROOT/etc/blacklist.conf"
 SCRIPT_NAME="$(basename $0)"
 
-# Custom list
-# Add custom domains here
+# Custom blocked domains
 CUSTOM_BLACKLIST=$(cat << EOF
-my.callhome.iot.trash.com
+my.iot.trash.callhome.com
 EOF
 )
 
 # Feed list
 # Expects file to contain one domain per line, else $PATTERN wont match.
-# Add '#' to beginning of line to disalba a feed.
+# Add '#' to beginning of line to temporary disable feed.
 URLS=$(cat << EOF
 https://pgl.yoyo.org/adservers/serverlist.php?hostformat=plain&showintro=0&mimetype=plaintext
 https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt
-https://block.energized.pro/ultimate/formats/domains.txt
 https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt
-https://mirror1.malwaredomains.com/files/justdomains
+#https://block.energized.pro/ultimate/formats/domains.txt
 #https://v.firebog.net/hosts/AdguardDNS.txt
 #https://v.firebog.net/hosts/Airelle-hrsk.txt
 #https://v.firebog.net/hosts/Airelle-trc.txt
@@ -118,44 +116,46 @@ init() {
 main() {
     [ "$1" == "init" ] && init
 
-    # Retrieve URLs and remove duplicates
+    # Retrieve block feeds
     echo "$URLS" | grep -vE '^#' | xargs ftp -o -  > $_tmpunsorted || \
-        error "Fetching URLs failed."
+        error "Fetching URLs failed"
+    logger "Block feeds fetched"
+
+    # Add custom domains
+    for name in $CUSTOM_BLACKLIST; do
+        echo "$name" >> $_tmpunsorted
+    done
+
+    # Sort and remove duplicates
     sort -fu $_tmpunsorted > $_tmpsorted
 
     # Create new empty blacklist
-    printf "# Managed by $SCRIPT_NAME\n# Last updated: $(date)\n" > $BLACKLIST_CONF
+    printf "# Managed by $SCRIPT_NAME\n# Last updated: $(date)\n" > $BLACKLIST_CONF || \
+        error "Failed writing to $BLACKLIST_CONF"
     
     # Validate domain names and create unbound conf
     while read name; do
         if echo "$name" | grep -oE "$PATTERN" 1>/dev/null; then
-            # Respond with NXDOMAIN for all blacklisted domains
             echo "local-zone: \"$name\" always_nxdomain" >> $BLACKLIST_CONF
         fi
     done < $_tmpsorted
 
-    # Add custom domains
-    # TODO: this is a hack. Fix this and include in domain validation before
-    # adding
-    for name in $CUSTOM_BLACKLIST; do
-        echo "local-zone: \"$name\" always_nxdomain" >> $BLACKLIST_CONF
-    done
-
     # Check configuration syntax. Empty blacklist and bail out on error
     if ! doas -u $UNBOUND_USER unbound-checkconf 1>/dev/null; then
         echo > $BLACKLIST_CONF
-        error "Syntax error in unbound configuration."
+        error "Syntax error in unbound configuration"
     fi
 
     # Reload server and conf
     # XXX: also flushes cache, we may want to dump and restore this
     if ! doas -u $UNBOUND_USER unbound-control reload 1>/dev/null; then
-        error "Reload unbound failed."
+        error "Reload unbound failed"
     fi
 
     # Log
     logger "$SCRIPT_NAME: Updated $BLACKLIST_CONF with $(wc -l $_tmpsorted | \
-        awk '{print $1}') entries"
+        awk '{print $1}') entries" 
 }
 
 main "$@"
+
