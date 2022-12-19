a057c60551eaeece5f2522ea060ed18d2d7f7460
elasmo <2766287+elasmo@users.noreply.github.com>
2022-12-19 14:57:59 +0100

    Update qemu-install.sh

--- qemu-install.sh
+++ qemu-install.sh
@@ -1,3 +1,9 @@
-qemu-img create -f qcow2 linux.img 30G
-qemu-system-x86_64 -cdrom <iso> -boot order=d -drive file=linux.img,format=qcow2 -m 3G
-qemu-system-x86_64 -m 3G -device virtio-vga,virgl=on -drive file=linux.img,format=qcow2,if=virtio -cpu host -smp 4 "$@"
+#!/bin/sh
+image="$1"
+iso="$2"
+memory="3G"
+disk="30G"
+
+qemu-img create -f qcow2 $image $disk
+qemu-system-x86_64 -cdrom $iso -boot order=d -drive file=$image,format=qcow2 -m $memory
+qemu-system-x86_64 -m $memory -device virtio-vga,virgl=on -drive file=$image,format=qcow2,if=virtio -cpu host -smp 4 "$image"
